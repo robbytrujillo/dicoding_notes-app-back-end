55b64b555cbd222eab76b34d9fd1bdfc5ff335d2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-07-27 10:08:52 +0700

    Kriteria Proyek

---
+++ kriteriaProyek/kriteriaProyek.js
@@ -0,0 +1,29 @@
+// Kriteria 1 - Web Server dapat menyimpan catatan
+// {
+//     id: string,
+//     title: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     tags: array of string,
+//     body: string,
+// },
+
+
+// {
+//     id: 'notes-V1StGXR8_Z5jdHi6B-myT',
+//     title: 'Sejarah JavaScript',
+//     createdAt: '2020-12-23T23:00:09.686Z',
+//     updatedAt: '2020-12-23T23:00:09.686Z',
+//     tags: ['NodeJS', 'JavaScript'],
+//     body: 'JavaScript pertama kali dikembangkan oleh Brendan Eich dari Netscape di bawah nama Mocha, yang nantinya namanya diganti menjadi LiveScript, dan akhirnya menjadi JavaScript. Navigator sebelumnya telah mendukung Java untuk lebih bisa dimanfaatkan para pemrogram yang non-Java.',
+//    },
+// {
+//     "title": "Judul Catatan",
+//     "tags": ["Tag 1", "Tag 2"],
+//     "body": "Konten catatan"
+//    }
+
+{
+    "status": "success",
+    "message": "Catatan berhasil ditambahkan", // optional message if there is any error or success status code 
+}
